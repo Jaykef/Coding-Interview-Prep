93a223ff2c69e4b226e29d540880739afa86fc3b
Jaward Sesay <jkef96@icloud.com>
2023-01-07 18:16:39 +0800

    Update MergeLinedLists.js

--- LeetCode Basic/MergeLinedLists.js
+++ LeetCode Basic/MergeLinedLists.js
@@ -10,6 +10,44 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
+
+// 1. Using Iteration
+var mergeKLists = function (lists) {
+    if (lists.length === 0) return null;
+
+    // Merge two sorted linked lists
+    function merge(l1, l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+
+        let head;
+        if (l1.val < l2.val) {
+            head = l1;
+            head.next = merge(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = merge(l1, l2.next);
+        }
+        return head;
+    }
+
+    // Iteratively merge the linked lists
+    while (lists.length > 1) {
+        const merged = [];
+        for (let i = 0; i < lists.length; i += 2) {
+            if (i + 1 < lists.length) {
+                merged.push(merge(lists[i], lists[i + 1]));
+            } else {
+                merged.push(lists[i]);
+            }
+        }
+        lists = merged;
+    }
+    return lists[0];
+}
+
+
+// 2. Covert list to array
 var mergeKLists = function(lists) {
     // Initialise an array to store the values in
     let mainArray = [];
