0d20f8f86bda1309cdff69a1185a8e823808df79
Jaward Sesay <jkef96@icloud.com>
2023-01-11 10:51:48 +0800

    Create ThreeSum.js

---
+++ LeetCode Basic/ThreeSum.js
@@ -0,0 +1,36 @@
+/**
+ * Jaykef - Jaward Sesay
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+function threeSum(nums) {
+    let result = [];
+    if (nums.length < 3) {
+        return result;
+    }
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+            let low = i + 1, high = nums.length - 1, sum = 0 - nums[i];
+            while (low < high) {
+                if (nums[low] + nums[high] === sum) {
+                    result.push([nums[i], nums[low], nums[high]]);
+                    while (low < high && nums[low] === nums[low + 1]) {
+                        low++;
+                    }
+                    while (low < high && nums[high] === nums[high - 1]) {
+                        high--;
+                    }
+                    low++;
+                    high--;
+                } else if (nums[low] + nums[high] < sum) {
+                    low++;
+                } else {
+                    high--;
+                }
+            }
+        }
+    }
+    return result;
+}
