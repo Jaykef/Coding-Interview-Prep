01d86a26a0a0770d7ba6b18ae55537cd71061526
Jaward Sesay <jkef96@icloud.com>
2023-01-12 11:13:45 +0800

    Create RemoveDupSortOrder.js

---
+++ LeetCode Basic/RemoveDupSortOrder.js
@@ -0,0 +1,15 @@
+/**
+ * Jaykef - Jaward Sesay
+ * @param {number[]} nums
+ * @return {number}
+ */
+function removeDuplicates(nums) {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
