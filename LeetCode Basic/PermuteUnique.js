12ef546f78fee044650a68bec06785ed355ce838
Jaward Sesay <jkef96@icloud.com>
2023-01-18 22:20:18 +0800

    Update PermuteUnique.js

--- LeetCode Basic/PermuteUnique.js
+++ LeetCode Basic/PermuteUnique.js
@@ -1,26 +1,31 @@
-function permuteUnique(nums) {
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
     let result = [];
-    let visited = new Set();
+    nums.sort();
+    let used = new Array(nums.length).fill(false);
     let current = [];
 
-    function backtrack(nums, current, visited) {
+    function backtrack(nums, current, used) {
         if (current.length === nums.length) {
             result.push(current.slice());
             return;
         }
 
         for (let i = 0; i < nums.length; i++) {
-            if (visited.has(i)) {
+            if (used[i] || (i > 0 && nums[i] === nums[i - 1] && !used[i - 1])) {
                 continue;
             }
-            visited.add(i);
+            used[i] = true;
             current.push(nums[i]);
-            backtrack(nums, current, visited);
+            backtrack(nums, current, used);
             current.pop();
-            visited.delete(i);
+            used[i] = false;
         }
     }
 
-    backtrack(nums, current, visited);
+    backtrack(nums, current, used);
     return result;
-}
+};
